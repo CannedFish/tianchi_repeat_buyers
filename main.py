8de5c04a4b4276b813057ce5d2e237f0c5c37df8
CannedFish <lianggy0719@126.com>
2018-02-01 11:03:33 +0800

    update score function to auc

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 # -*- coding: utf8 -*-
 
-import warnings
+import sys
+import logging
 
 import pandas as pd
 import numpy as np
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import brier_score_loss
-from sklearn.exceptions import ConvergenceWarning
+from sklearn.metrics import brier_score_loss, roc_auc_score
+
+class StreamToLogger(object):
+    """
+    Fake file-like stream object that redirects writes to a logger instance.
+    """
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s',
+    filename="out.log",
+    filemode='w'
+)
+
+stderr_logger = logging.getLogger('STDERR')
+sl = StreamToLogger(stderr_logger, logging.ERROR)
+sys.stderr = sl
 
 global v
 
@@ -17,40 +41,33 @@ def train():
     y_train = pd.read_csv('./inter/%s/label_train_db.csv' % v)
     y_test = pd.read_csv('./inter/%s/label_test_db.csv' % v)
 
-    lr = None
-    with warnings.catch_warnings():
-        warnings.filterwarnings('error')
+    iter_num = 200
+    lr = LogisticRegression(solver='sag', max_iter=iter_num, n_jobs=-1, warm_start=True)
         
-        # 1. create lr model with different parameters
-        # 2. train and test
-        # 3. use the model with the best score
-        need = True
-        iter_num = 200
-        while need:
-            try:
-                # TODO: try ues warm_start parameter
-                lr = LogisticRegression(solver='sag', max_iter=iter_num, n_jobs=-1)
-                print "Start train through LogisticRegression model..."
-                print lr
-                lr.fit(X_train, y_train.values.ravel())
-            except ConvergenceWarning, w:
-                print w
-                iter_num += 200
-            else:
-                need = False
-            # finally:
-                # print "Finish training"
-                # y_test_prob = lr.predict_proba(X_test)
-                # print "Test score: %f" % brier_score_loss(y_test.values.ravel(), y_test_prob[:,1])
+    # 1. create lr model with different parameters
+    # 2. train and test
+    # 3. use the model with the best score
+    print "\nStart train through LogisticRegression model..."
+    print lr
+    need = True
+    batch = 0
+    while need:
+        print "*****************************************************************"
+        print "Start batch %d" % batch
+        lr.fit(X_train, y_train.values.ravel())
+        print "Finish training with %s" % lr.n_iter_
+        y_test_prob = lr.predict_proba(X_test)
+        # print "Test score: %f" % brier_score_loss(y_test.values.ravel(), y_test_prob[:,1])
+        print "Test score: %f" % roc_auc_score(y_test.values.ravel(), y_test_prob[:,1])
+        batch += 1
 
-    print "Finish training"
-    y_test_prob = lr.predict_proba(X_test)
-    print "Test score: %f" % brier_score_loss(y_test.values.ravel(), y_test_prob[:,1])
+        if lr.n_iter_[0] != iter_num:
+            need = False
     
     return lr
 
 def predict(model):
-    print "Start generate the predicted result..."
+    print "\nStart generate the predicted result..."
     X = pd.read_csv('./inter/%s/feature_predict_db.csv' % v)
     y_pred = model.predict_proba(X)
 
